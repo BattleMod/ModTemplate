93a94aabb31f9557b31be5b2628eebbc4074ed15
Ryhon <ryhon@ryhn.link>
2023-08-13 00:46:47 +0200

    Initial commit

---
+++ Plugin.cs
@@ -0,0 +1,16 @@
+﻿using BepInEx;
+using BepInEx.Unity.IL2CPP;
+
+namespace ModTemplate;
+
+[BepInPlugin("link.ryhn.battlemod.modtemplate", "Mod Template", "1.0.0.0")]
+public class Plugin : BasePlugin
+{
+	public static Plugin Instance;
+
+	public override void Load()
+	{
+		Instance = this;
+		Log.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+	}
+}
